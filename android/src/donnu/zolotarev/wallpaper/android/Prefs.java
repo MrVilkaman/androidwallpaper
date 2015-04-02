a2fe54a9144a6fdc472c04f11ff6152c36d8fdef
Zahar <zahar.zolotarev@gmail.com>
2015-04-03 01:24:15 +0300

    Added buttons

--- android/src/donnu/zolotarev/wallpaper/android/Prefs.java
+++ android/src/donnu/zolotarev/wallpaper/android/Prefs.java
@@ -1,13 +1,22 @@
 package donnu.zolotarev.wallpaper.android;
 
+import android.app.Activity;
 import android.app.WallpaperManager;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.LabeledIntent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Prefs extends PreferenceActivity {
@@ -29,13 +38,65 @@ public class Prefs extends PreferenceActivity {
     } // onCreate
 
     public static class MyPreferenceFragment extends PreferenceFragment{
-    	
+
+        private static final String APP_PNAME = "market://details?id=donnu.zolotarev.rushgame.android";
+        private static final String MORE_APPS = "market://search?q=Zahar+Zolotarev";
+
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.prefs);
+
+            rateUs();
+            moreApps();
+            shareIt();
+            setWallpaperButton();
+        }
+
+        private void moreApps() {
+
+            Preference button = (Preference)getPreferenceManager().findPreference("more_apps");
+            if (button != null) {
+                button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(MORE_APPS)));
+                        return false;
+                    }
+                });
+            }
+        }
+
+        private void shareIt() {
+            Preference button = (Preference)getPreferenceManager().findPreference("share");
+            if (button != null) {
+                button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
+                       share(getActivity(),getString(R.string.share_text));
+                        return false;
+                    }
+                });
+            }
+        }
+
+        private void rateUs() {
+            Preference button = (Preference)getPreferenceManager().findPreference("rateus");
+            if (button != null) {
+                button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(APP_PNAME)));
+                        return false;
+                    }
+                });
+            }
+
+        }
+
+        private void setWallpaperButton() {
             Preference button = (Preference)getPreferenceManager().findPreference("setwall");
-            if (button != null)
+            if (button != null) {
                 button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                     @Override
                     public boolean onPreferenceClick(Preference arg0) {
@@ -50,11 +111,55 @@ public class Prefs extends PreferenceActivity {
                             i.setAction(WallpaperManager.ACTION_LIVE_WALLPAPER_CHOOSER);
                         }
                         startActivityForResult(i, 0);
-                        return true;
+                        return false;
                     }
                 });
+            }
         }
-        
+
     } // MyPreferenceFragment
 
+
+    public static void share(Activity context, String text){
+
+        Resources resources = context.getResources();
+
+        Intent emailIntent = new Intent();
+        emailIntent.setAction(Intent.ACTION_SEND);
+        // Native email client doesn't currently support HTML, but it doesn't hurt to try in case they fix it
+        emailIntent.putExtra(Intent.EXTRA_TEXT, text); /*Html.fromHtml(resources.getString(R.string.share_email_native)));*/
+//        emailIntent.putExtra(Intent.EXTRA_SUBJECT, resources.getString(R.string.share_email_subject));
+        emailIntent.setType("message/rfc822");
+
+        PackageManager pm = context.getPackageManager();
+        Intent sendIntent = new Intent(Intent.ACTION_SEND);
+        sendIntent.setType("text/plain");
+
+
+        Intent openInChooser = Intent.createChooser(emailIntent, resources.getString(R.string.share_chooser_text));
+
+        List<ResolveInfo> resInfo = pm.queryIntentActivities(sendIntent, 0);
+        List<LabeledIntent> intentList = new ArrayList<LabeledIntent>();
+        for (int i = 0; i < resInfo.size(); i++) {
+            // Extract the label, append it, and repackage it in a LabeledIntent
+            ResolveInfo ri = resInfo.get(i);
+            String packageName = ri.activityInfo.packageName;
+            if(packageName.contains("android.email")) {
+                emailIntent.setPackage(packageName);
+            } else if(packageName.contains("twitter") || packageName.contains("facebook") || packageName.contains("mms") || packageName.contains("android.gm")) {
+                Intent intent = new Intent();
+                intent.setComponent(new ComponentName(packageName, ri.activityInfo.name));
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, text);
+                intentList.add(new LabeledIntent(intent, packageName, ri.loadLabel(pm), ri.icon));
+            }
+        }
+
+        // convert intentList to array
+        LabeledIntent[] extraIntents = intentList.toArray( new LabeledIntent[ intentList.size() ]);
+
+        openInChooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, extraIntents);
+        context.startActivity(openInChooser);
+    }
 } // Prefs
