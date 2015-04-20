eededf837380780f92aa8e53b8221d2f6bf10f47
Zahar <zahar.zolotarev@gmail.com>
2015-04-20 23:38:16 +0300

    fix share dialog

--- android/src/donnu/zolotarev/wallpaper/android/utils/Utils.java
+++ android/src/donnu/zolotarev/wallpaper/android/utils/Utils.java
@@ -1,16 +1,9 @@
 package donnu.zolotarev.wallpaper.android.utils;
 
 import android.app.Activity;
-import android.content.ComponentName;
 import android.content.Intent;
-import android.content.pm.LabeledIntent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
 import android.net.Uri;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import donnu.zolotarev.wallpaper.android.R;
 
@@ -23,11 +16,17 @@ public class Utils {
     public static void share(Activity context, String text){
 
         Resources resources = context.getResources();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        context.startActivity(Intent.createChooser(intent, resources.getString(R.string.share_chooser_text)));
+
+      /*
 
         Intent emailIntent = new Intent();
         emailIntent.setAction(Intent.ACTION_SEND);
         // Native email client doesn't currently support HTML, but it doesn't hurt to try in case they fix it
-        emailIntent.putExtra(Intent.EXTRA_TEXT, text); /*Html.fromHtml(resources.getString(R.string.share_email_native)));*/
+        emailIntent.putExtra(Intent.EXTRA_TEXT, text); *//*Html.fromHtml(resources.getString(R.string.share_email_native)));*//*
 //        emailIntent.putExtra(Intent.EXTRA_SUBJECT, resources.getString(R.string.share_email_subject));
         emailIntent.setType("message/rfc822");
 
@@ -60,6 +59,6 @@ public class Utils {
         LabeledIntent[] extraIntents = intentList.toArray( new LabeledIntent[ intentList.size() ]);
 
         openInChooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, extraIntents);
-        context.startActivity(openInChooser);
+        context.startActivity(openInChooser);*/
     }
 }
